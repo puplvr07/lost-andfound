78232c4c7e006eeb744a9ead6c73e4f85446deba
Sean <seanroscady@gmail.com>
2026-09-08 10:35:16 +0800

    customized styling

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: "#232b4e",
+                    light: "#356cbe",
+                },
+                secondary: "#48bdec",
+                "background-light": "#f8fafc",
+            },
+            fontFamily: {
+                display: ["Quicksand", "sans-serif"],
+            },
+            borderRadius: {
+                DEFAULT: "0.5rem",
+                xl: "1rem",
+            },
+        },
+    },
+};
